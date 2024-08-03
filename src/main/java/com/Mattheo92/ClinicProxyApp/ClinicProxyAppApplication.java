4182332bf728e7f62ce0eb43db7777e1a5ea86ff
Mattheo992 <stepniak.matt@gmail.com>
2024-08-03 16:16:10 +0200

    Initial commit

---
+++ src/main/java/com/Mattheo92/ClinicProxyApp/ClinicProxyAppApplication.java
@@ -0,0 +1,13 @@
+package com.Mattheo92.ClinicProxyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicProxyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicProxyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mattheo92/ClinicProxyApp/ClinicProxyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mattheo92.ClinicProxyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicProxyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
